ae33dee25cf348160954f0c64ce2825fad672a4f
Shreya-CS <shreyashanmugasundaram@gmail.com>
2023-04-17 23:24:07 +0530

    media_screen_upto_phone

--- main.js
+++ main.js
@@ -31,3 +31,22 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+//show/hide nav menu when adjusted to media screen
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+//closing nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav)
